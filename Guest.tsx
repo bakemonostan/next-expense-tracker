f8234ceb174d658a9a824eb613783926a7924ca4
bakemonostan <ehizomokhomion@gmail.com>
2024-06-25 20:36:38 +0100

    add form actions and add transaction form

--- Guest.tsx
+++ Guest.tsx
@@ -1,3 +1,13 @@
-export default function Guest() {
-  return <div>Guest</div>;
-}
+import { SignInButton } from "@clerk/nextjs";
+
+const Guest = () => {
+  return (
+    <div className="guest">
+      <h1>Welcome</h1>
+      <p>Please sign in to manage your transactions</p>
+      <SignInButton />
+    </div>
+  );
+};
+
+export default Guest;
