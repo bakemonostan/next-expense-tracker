1dae1706d61ab8b143f585a3f8a7c2474cf03be6
bakemonostan <ehizomokhomion@gmail.com>
2024-06-25 20:52:55 +0100

    add data for balance and expenses and create components for each

--- Balance.tsx
+++ Balance.tsx
@@ -1,3 +1,15 @@
-export default function Balance() {
-  return <div>Balance</div>;
-}
+import getUserBalance from "../actions/getUserBalance";
+import { addCommas } from "../lib/utils";
+
+const Balance = async () => {
+  const { balance } = await getUserBalance();
+
+  return (
+    <>
+      <h4>Your Balance</h4>
+      <h1>${addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
+    </>
+  );
+};
+
+export default Balance;
